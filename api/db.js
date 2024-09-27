7bd4264a12e1c3cc8e2bfe0193c9808bd9c8277b
Indominous <93007035+Indominous1@users.noreply.github.com>
2024-09-27 16:16:28 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,8 +1,9 @@
 import mysql from "mysql"
+//Add your personal data over here provide password and database name correctly if using localhost
 
 export const db = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "ankur",
+    password: "password",
     database: "blog"
 })
